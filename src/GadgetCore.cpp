3739f3827ce7d00f084ec591ab0ce21e02f01a60
ShikenNuggets <carter.rennick@gmail.com>
2025-11-13 21:39:20 -0500

    Setup a tests executable with Catch2

--- src/GadgetCore.cpp
+++ src/GadgetCore.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main()
+int NotMain()
 {
 	cout << "Hello CMake." << endl;
 	return 0;
---
+++ tests/GadgetCoreTests.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("Sanity check") {
+    REQUIRE(1 + 1 == 2);
+}
