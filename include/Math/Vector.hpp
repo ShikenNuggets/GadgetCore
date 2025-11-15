da19567622f30b1246c6f37a0c342c756a67f98a
ShikenNuggets <carter.rennick@gmail.com>
2025-11-15 12:07:34 -0500

    TVec3 and 4 stubs

--- include/Math/Vector.hpp
+++ include/Math/Vector.hpp
@@ -7,8 +7,7 @@ namespace GCore
 	template<Math::FloatLike T>
 	struct TVec2
 	{
-		T x;
-		T y;
+		T x, y;
 		
 		explicit constexpr TVec2(T x_ = 0.0, T y_ = 0.0) : x(x_), y(y_){}
 
@@ -22,5 +21,23 @@ namespace GCore
 		inline constexpr TVec2 operator /(T value_) const{ return TVec2(Math::SafeDivide(x, value_), Math::SafeDivide(y, value_)); }
 	};
 
+	template<Math::FloatLike T>
+	struct TVec3
+	{
+		T x, y, z;
+
+		explicit constexpr TVec3(T x_ = 0.0, T y_ = 0.0, T z_ = 0.0) : x(x_), y(y_), z(z_){}
+	};
+
+	template<Math::FloatLike T>
+	struct TVec4
+	{
+		T x, y, z, w;
+
+		explicit constexpr TVec4(T x_ = 0.0, T y_ = 0.0, T z_ = 0.0, T w_ = 0.0) : x(x_), y(y_), z(z_), w(w_){}
+	};
+
 	using Vector2 = TVec2<Math::GFloat>;
+	using Vector3 = TVec3<Math::GFloat>;
+	using Vector4 = TVec4<Math::GFloat>;
 }
--- tests/Math/VectorTests.cpp
+++ tests/Math/VectorTests.cpp
@@ -2,16 +2,45 @@
 
 #include "Math/Vector.hpp"
 
-TEST_CASE("TVec2 Default Constructor", "[tvec2_default_constructor]")
+using namespace GCore;
+
+// --------------------------- //
+// ---------- TVec2 ---------- //
+// --------------------------- //
+TEST_CASE("TVec2::TVec2")
 {
-	const GCore::TVec2<double> doubleVec2;
+	const TVec2<double> doubleVec2;
 	REQUIRE(doubleVec2.x == 0.0);
 	REQUIRE(doubleVec2.y == 0.0);
 }
 
-TEST_CASE("TVec2::Zero", "[tvec2::zero]")
+TEST_CASE("TVec2::Zero")
 {
-	const GCore::TVec2<double> zeroVec = GCore::TVec2<double>::Zero();
+	const TVec2<double> zeroVec = TVec2<double>::Zero();
 	REQUIRE(zeroVec.x == 0.0);
 	REQUIRE(zeroVec.y == 0.0);
+}
+
+// --------------------------- //
+// ---------- TVec3 ---------- //
+// --------------------------- //
+TEST_CASE("TVec3::TVec3")
+{
+	const TVec3<double> doubleVec3;
+	REQUIRE(doubleVec3.x == 0.0);
+	REQUIRE(doubleVec3.y == 0.0);
+	REQUIRE(doubleVec3.z == 0.0);
+}
+
+// --------------------------- //
+// ---------- TVec4 ---------- //
+// --------------------------- //
+
+TEST_CASE("TVec4::TVec4")
+{
+	const TVec4<double> doubleVec4;
+	REQUIRE(doubleVec4.x == 0.0);
+	REQUIRE(doubleVec4.y == 0.0);
+	REQUIRE(doubleVec4.z == 0.0);
+	REQUIRE(doubleVec4.w == 0.0);
 }
