fd671ad2b81b06f73ba0d8a71ddef785881c87e0
ShikenNuggets <carter.rennick@gmail.com>
2025-11-23 16:12:12 -0500

    Added a numeric concept for non-floats

--- include/GCore/Math/Math.hpp
+++ include/GCore/Math/Math.hpp
@@ -1,16 +1,29 @@
 #pragma once
 
 #include <numbers>
+#include <type_traits>
 
 namespace GCore::Math
 {
 	template<typename T>
 	concept FloatLike = std::is_floating_point_v<T> && std::is_convertible_v<T, double>;
-
-	using GFloat = double;
 	static_assert(FloatLike<float>, "Sanity Check - Built-in float must satisfy the FloatLike concept");
 	static_assert(FloatLike<double>, "Sanity Check - Built-in double must satisfy the FloatLike concept");
 	static_assert(FloatLike<long double>, "Sanity Check - Built-in long double must satisfy the FloatLike concept");
+
+	template <typename T>
+	concept Numeric = std::is_arithmetic_v<T> and not std::is_same_v<T, bool> and not std::is_same_v<T, char> and not std::is_same_v<T, wchar_t>;
+	static_assert(Numeric<int>, "Sanity Check - int must satisfy the Numeric concept");
+	static_assert(Numeric<int8_t>, "Sanity Check - int8_t must satisfy the Numeric concept");
+	static_assert(Numeric<uint8_t>, "Sanity Check - uint8_t must satisfy the Numeric concept");
+	static_assert(Numeric<uint64_t>, "Sanity Check - uint64_t must satisfy the Numeric concept");
+	static_assert(Numeric<float>, "Sanity Check - float must satisfy the Numeric concept");
+	static_assert(!Numeric<bool>, "Sanity Check - bool is not numeric");
+	static_assert(!Numeric<char>, "Sanity Check - char should not be used as a numeric type");
+	static_assert(!Numeric<wchar_t>, "Sanity Check - wchar_t should not be used as a numeric type");
+
+	using GFloat = double;
+	static_assert(Numeric<GFloat>, "GFloat must be numeric type");
 	static_assert(FloatLike<GFloat>, "GFloat must be a floating point type and implicitly-convertible to double");
 
 	template<FloatLike T>
