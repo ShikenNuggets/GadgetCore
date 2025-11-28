5d4608bb1dc4cf8a3c3a848833752bec116cc861
ShikenNuggets <carter.rennick@gmail.com>
2025-11-27 20:49:05 -0500

    Up/Forward/Right vectors

--- include/GCore/Math/TVec3.hpp
+++ include/GCore/Math/TVec3.hpp
@@ -13,5 +13,10 @@ namespace Gadget
 		explicit constexpr TVec3(T x_ = 0.0, T y_ = 0.0, T z_ = 0.0) noexcept : x(x_), y(y_), z(z_){}
 
 		static inline constexpr TVec3 Fill(T value) noexcept{ return TVec3(value, value, value); }
+
+		// Sorry for baking in these assumptions, but it's useful
+		static inline constexpr TVec3 Up() noexcept{ return TVec3(0.0, 1.0, 0.0); }
+		static inline constexpr TVec3 Forward() noexcept{ return TVec3(0.0, 0.0, -1.0); }
+		static inline constexpr TVec3 Right() noexcept{ return TVec3(1.0, 0.0, 0.0); }
 	};
 }
