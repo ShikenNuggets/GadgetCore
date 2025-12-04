d68072f091ee1bb9abca6118fb9c9cbcabe81c54
ShikenNuggets <carter.rennick@gmail.com>
2025-12-03 19:40:50 -0500

    TQuat::Identity

--- include/GCore/Math/Quaternion.hpp
+++ include/GCore/Math/Quaternion.hpp
@@ -7,9 +7,11 @@ namespace Gadget
 	template <typename T>
 	struct TQuat
 	{
-		T x, y, z, w;
+		T w, x, y, z;
 
-		explicit TQuat(T x_ = 0.0, T y_ = 0.0, T z_ = 0.0, T w_ = 0.0) : x(x_), y(y_), z(z_), w(w_){}
+		explicit constexpr TQuat(T w_ = 1.0, T x_ = 0.0, T y_ = 0.0, T z_ = 0.0) noexcept : w(w_), x(x_), y(y_), z(z_){}
+
+		static inline constexpr TQuat Identity() noexcept{ return TQuat(1.0, 0.0, 0.0, 0.0); }
 	};
 
 	using Quaternion = TQuat<Math::GFloat>;
--- tests/Math/QuaternionTests.cpp
+++ tests/Math/QuaternionTests.cpp
@@ -6,9 +6,24 @@ using namespace Gadget;
 
 TEST_CASE("TQuat::TQuat", "[tquat_constructor]")
 {
-	const TQuat<double> doubleQuat;
+	const auto defaultInit = TQuat<double>();
+	REQUIRE(defaultInit.w == 1.0);
+	REQUIRE(defaultInit.x == 0.0);
+	REQUIRE(defaultInit.y == 0.0);
+	REQUIRE(defaultInit.z == 0.0);
+
+	const auto customInit = TQuat<double>(1.0, 2.0, 3.0, 4.0);
+	REQUIRE(customInit.w == 1.0);
+	REQUIRE(customInit.x == 2.0);
+	REQUIRE(customInit.y == 3.0);
+	REQUIRE(customInit.z == 4.0);
+}
+
+TEST_CASE("TQuat::Identity", "[tquat_identity]")
+{
+	const TQuat<double> doubleQuat = TQuat<double>::Identity();
+	REQUIRE(doubleQuat.w == 1.0);
 	REQUIRE(doubleQuat.x == 0.0);
 	REQUIRE(doubleQuat.y == 0.0);
 	REQUIRE(doubleQuat.z == 0.0);
-	REQUIRE(doubleQuat.w == 0.0);
 }
