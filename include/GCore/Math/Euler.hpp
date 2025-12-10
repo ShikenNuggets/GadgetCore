1cd87add2d58cb88b7cfd100f8d31618ac897008
ShikenNuggets <carter.rennick@gmail.com>
2025-12-09 21:30:43 -0500

    Euler lerp

--- include/GCore/Math/Euler.hpp
+++ include/GCore/Math/Euler.hpp
@@ -13,6 +13,14 @@ namespace Gadget
 		explicit constexpr TEuler(T x_ = 0.0, T y_ = 0.0, T z_ = 0.0) noexcept : x(x_), y(y_), z(z_){}
 		explicit TEuler(const TVec3<T>& v) noexcept : x(v.x), y(v.y), z(v.z){}
 
+		inline constexpr TEuler operator+(const TEuler& e) const noexcept{ return TEuler(x + e.x, y + e.y, z + e.z); }
+		inline constexpr TEuler operator-(const TEuler& e) const noexcept{ return TEuler(x - e.x, y - e.y, z - e.z); }
+		inline constexpr TEuler operator*(T value) const noexcept{ return TEuler(x * value, y * value, z * value); }
+
+		inline friend constexpr TEuler operator*(float value, const TEuler& e) noexcept{ return e * value; }
+
+		static inline constexpr TEuler Lerp(const TEuler& a, const TEuler& b, T t){ return a + t * (b - a); }
+
 		inline constexpr bool IsValid() const{ return Math::IsValidNumber(x) && Math::IsValidNumber(y) && Math::IsValidNumber(z); }
 	};
 
--- tests/Math/EulerTests.cpp
+++ tests/Math/EulerTests.cpp
@@ -3,6 +3,7 @@
 #include "GCore/Math/Euler.hpp"
 
 using namespace Gadget;
+using Catch::Approx;
 
 TEST_CASE("TEuler::TEuler", "[teuler_constructor]")
 {
@@ -20,6 +21,50 @@ TEST_CASE("TEuler::TEuler(TVec3)", "[teuler_constructor_tvec3]")
 	REQUIRE(euler.x == doubleVec3.x);
 	REQUIRE(euler.y == doubleVec3.y);
 	REQUIRE(euler.z == doubleVec3.z);
+}
+
+TEST_CASE("TEuler::+", "[teuler_operator_+]")
+{
+	const auto plusTest = TEuler<double>(1.0, 2.0, 3.0) + TEuler<double>(4.0, 5.0, 6.0);
+	REQUIRE(plusTest.x == 5.0);
+	REQUIRE(plusTest.y == 7.0);
+	REQUIRE(plusTest.z == 9.0);
+}
+
+TEST_CASE("TEuler::-", "[teuler_operator_-]")
+{
+	const auto subTest = TEuler<double>(6.0, 5.0, 4.0) - TEuler<double>(3.0, 2.0, 1.0);
+	REQUIRE(subTest.x == 3.0);
+	REQUIRE(subTest.y == 3.0);
+	REQUIRE(subTest.z == 3.0);
+}
+
+TEST_CASE("TEuler::*", "[teuler_operator_*]")
+{
+	constexpr auto x = 1.0;
+	constexpr auto y = 2.0;
+	constexpr auto z = 3.0;
+
+	const auto multTest = TEuler<double>(x, y, z) * 4.0;
+	REQUIRE(multTest.x == 4.0);
+	REQUIRE(multTest.y == 8.0);
+	REQUIRE(multTest.z == 12.0);
+
+	const auto multTest2 = 4.0 * TEuler<double>(x, y, z);
+	REQUIRE(multTest.x == multTest2.x);
+	REQUIRE(multTest.y == multTest2.y);
+	REQUIRE(multTest.z == multTest2.z);
+}
+
+TEST_CASE("TEuler::Lerp", "[teuler_lerp]")
+{
+	const auto a = TEuler<double>(0.0, 1.0, 2.0);
+	const auto b = TEuler<double>(4.0, 5.0, 6.0);
+	const auto lerpTest = TEuler<double>::Lerp(a, b, 0.5);
+
+	REQUIRE(lerpTest.x == Approx(2.0));
+	REQUIRE(lerpTest.y == Approx(3.0));
+	REQUIRE(lerpTest.z == Approx(4.0));
 }
 
 TEST_CASE("TEuler::IsValid", "[teuler_is_valid]")
