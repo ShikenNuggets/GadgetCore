654223166e62258bd47e60b737f600f93934b6a4
ShikenNuggets <carter.rennick@gmail.com>
2026-03-14 09:38:01 -0400

    MouseToButtonId test

--- tests/ThirdParty/SDL_UtilsTests.cpp
+++ tests/ThirdParty/SDL_UtilsTests.cpp
@@ -15,3 +15,12 @@ TEST_CASE("SDL_Utils::KeycodeToButtonId", "[sdl_utils_keycode_to_button_id]")
 	REQUIRE(SDL_Utils::KeycodeToButtonId(SDLK_F1) == ButtonId::Keyboard_F1);
 	REQUIRE(SDL_Utils::KeycodeToButtonId(SDLK_F12) == ButtonId::Keyboard_F12);
 }
+
+TEST_CASE("SDL_Utils::MouseToButtonId", "[sdl_utils_mouse_to_button_id]")
+{
+	REQUIRE(SDL_Utils::MouseToButtonId(SDL_BUTTON_LEFT) == ButtonId::Mouse_Left);
+	REQUIRE(SDL_Utils::MouseToButtonId(SDL_BUTTON_MIDDLE) == ButtonId::Mouse_Middle);
+	REQUIRE(SDL_Utils::MouseToButtonId(SDL_BUTTON_RIGHT) == ButtonId::Mouse_Right);
+	REQUIRE(SDL_Utils::MouseToButtonId(SDL_BUTTON_X1) == ButtonId::Mouse_Button_4);
+	REQUIRE(SDL_Utils::MouseToButtonId(SDL_BUTTON_X2) == ButtonId::Mouse_Button_5);
+}
