f2e963a32372918fc83a068faea3bf8555150c94
ShikenNuggets <carter.rennick@gmail.com>
2026-03-13 21:23:48 -0400

    Allow demo to be selected on the command line

--- demos/DemosMain.cpp
+++ demos/DemosMain.cpp
@@ -5,16 +5,24 @@
 
 int main(int argc, char* argv[])
 {
-	std::println("Pick a Demo: ");
-	std::println("[1] Basic Window");
-
 	std::string input;
-	std::cin >> input;
+	if (argc > 1)
+	{
+		input = std::span<char*>(argv, argc)[1];
+	}
+
+	if (input.empty())
+	{
+		std::println("Pick a Demo: ");
+		std::println("[1] Basic Window");
+		std::cin >> input;
+	}
 
 	if (input.contains('1'))
 	{
 		return GadgetCoreDemos::Demo1();
 	}
-
+	
+	std::println("No valid demo selected, exiting.");
 	return 0;
 }
