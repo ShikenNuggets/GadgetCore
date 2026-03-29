fcb4b6c76525c41c2ef16e57150a7771674593ae
ShikenNuggets <carter.rennick@gmail.com>
2026-03-29 13:19:44 -0400

    Adjusted timer tests to be more forgiving of slower, higher precision machines

--- tests/TimerTests.cpp
+++ tests/TimerTests.cpp
@@ -8,19 +8,20 @@ using namespace std::chrono_literals;
 using namespace Gadget;
 using Catch::Approx;
 
+// TODO - I'm not sure if these are actually a good idea
 TEST_CASE("Timer::DeltaSeconds", "[timer_delta_seconds]")
 {
 	Timer timer;
 	REQUIRE(timer.DeltaSeconds() == Approx(0.0));
-	REQUIRE(timer.SecondsSinceLastUpdate() == Approx(0.0));
 	REQUIRE(timer.SecondsSinceStart() == Approx(0.0));
+	REQUIRE(timer.SecondsSinceLastUpdate() <= Approx(0.001));
 
 	std::this_thread::sleep_for(1ms);
 
 	timer.Update();
 	REQUIRE(timer.DeltaSeconds() >= Approx(0.001));
-	REQUIRE(timer.SecondsSinceLastUpdate() == Approx(0.0));
 	REQUIRE(timer.SecondsSinceStart() >= Approx(0.001));
+	REQUIRE(timer.SecondsSinceLastUpdate() <= Approx(0.001));
 
 	std::this_thread::sleep_for(2ms);
 	REQUIRE(timer.SecondsSinceLastUpdate() >= Approx(0.002));
