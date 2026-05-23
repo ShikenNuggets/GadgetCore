30a1ef621dc8d217788d9f521cacf24387748e11
ShikenNuggets <carter.rennick@gmail.com>
2026-05-23 19:27:51 -0400

    Slight improvement to random seed quality when random_device is not supported

--- src/Random.cpp
+++ src/Random.cpp
@@ -18,6 +18,8 @@ Random::SeedT Random::TrueRandomValue() noexcept
 	}
 #endif //GADGET_PLATFORM_SUPPORTS_RANDOM_DEVICE
 
-	// Fall back to time-based seed if hardware doesn't support random_device 
-	return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+	// Fall back to time-based seed if hardware doesn't support random_device
+	auto nanoTime = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+	static_assert(sizeof(nanoTime) >= 8); // Sanity check: the following code is UB if this was ever not true
+	return static_cast<Random::SeedT>(nanoTime ^ (nanoTime >> 32)); // This ensures we use every bit from the timestamp even though the result is 32 bit
 }
