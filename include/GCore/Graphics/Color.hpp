53a8c7ba3e2a4c56596adba71a8158b0f3309a21
ShikenNuggets <carter.rennick@gmail.com>
2026-08-19 22:03:27 -0400

    Made DarkGray a bit darker

--- include/GCore/Graphics/Color.hpp
+++ include/GCore/Graphics/Color.hpp
@@ -71,7 +71,7 @@ namespace Gadget
 		[[nodiscard]] static inline constexpr auto Pink()		{ return Color(1.0f, 0.0f, 1.0f, 1.0f); }
 		[[nodiscard]] static inline constexpr auto Brown()		{ return Color(0.4f, 0.2f, 0.0f, 1.0f); }
 		[[nodiscard]] static inline constexpr auto Gray()		{ return Color(0.5f, 0.5f, 0.5f, 1.0f); }
-		[[nodiscard]] static inline constexpr auto DarkGray()	{ return Color(0.2f, 0.2f, 0.2f, 1.0f); }
+		[[nodiscard]] static inline constexpr auto DarkGray()	{ return Color(0.1f, 0.1f, 0.1f, 1.0f); }
 
 		[[nodiscard]] inline bool IsValid() const
 		{
