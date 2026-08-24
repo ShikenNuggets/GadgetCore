2d6e3656b51aa6b28a75d127c1f80af156a4c60f
ShikenNuggets <carter.rennick@gmail.com>
2026-08-23 21:16:16 -0400

    Compile shaders in CMake

--- cmake/GadgetCoreUtils.cmake
+++ cmake/GadgetCoreUtils.cmake
@@ -34,3 +34,54 @@ function(GadgetCore_NoDiscardAsError target)
 		$<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:-Werror=unused-result>
 	)
 endfunction()
+
+function(GadgetCore_CompileShaders TARGET_NAME)
+	set(options "")
+	set(oneValueArgs SRC_DIR OUT_DIR)
+	set(multiValueArgs FLAGS)
+	cmake_parse_arguments(PARSE_ARGV 1 ARG "${options}" "${oneValueArgs}" "${multiValueArgs}")
+
+	if(NOT ARG_SRC_DIR OR NOT ARG_OUT_DIR)
+		message(FATAL_ERROR "GadgetCore_CompileShaders requires both SRC_DIR and OUT_DIR to be specified.")
+	endif()
+
+	find_package(Vulkan REQUIRED QUIET)
+	if(NOT Vulkan_GLSLC_EXECUTABLE)
+		message(WARNING "glslc compiler not found! Check your Vulkan SDK installation if you require SPIR-V compilation.") # Not a fatal error since we support many platforms/use-cases that won't need this
+		return()
+	endif()
+
+	file(GLOB_RECURSE SHADERS CONFIGURE_DEPENDS "${ARG_SRC_DIR}/*.glsl")
+	if(NOT SHADERS)
+		message(WARNING "No .glsl files found in ${ARG_SRC_DIR}")
+		return()
+	endif()
+
+	file(MAKE_DIRECTORY ${ARG_OUT_DIR})
+	set(SPV_OUTPUTS "")
+
+	foreach(SHADER IN LISTS SHADERS)
+		file(RELATIVE_PATH REL_PATH "${ARG_SRC_DIR}" "${SHADER}")
+
+		get_filename_component(REL_DIR "${REL_PATH}" DIRECTORY)
+        get_filename_component(SHADER_NAME "${REL_PATH}" NAME_WLE)
+
+		set(TARGET_OUT_DIR "${ARG_OUT_DIR}/${REL_DIR}")
+        file(MAKE_DIRECTORY "${TARGET_OUT_DIR}")
+
+		set(SPV_FILE "${TARGET_OUT_DIR}/${SHADER_NAME}.spv")
+		
+		add_custom_command(
+			OUTPUT ${SPV_FILE}
+			COMMAND ${Vulkan_GLSLC_EXECUTABLE} ${ARG_FLAGS} ${SHADER} -o ${SPV_FILE}
+			DEPENDS ${SHADER}
+			COMMENT "Compiling ${REL_PATH}..."
+		)
+		
+		list(APPEND SPV_OUTPUTS ${SPV_FILE})
+	endforeach()
+
+	set(SHADER_TARGET "${TARGET_NAME}_Shaders")
+	add_custom_target(${SHADER_TARGET} DEPENDS ${SPV_OUTPUTS})
+	add_dependencies(${TARGET_NAME} ${SHADER_TARGET})
+endfunction()
